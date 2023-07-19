28f35d5b0d3455d45625c7613ca36d69ff0dd24f
PravinMane557 <pravinmane557@gmail.com>
2023-07-19 19:33:26 +0530

    JS Assignment 01
    JS Assignment 01 -Variable declaration and initialization

--- 01_variableAssign.js
+++ 01_variableAssign.js
@@ -1,5 +1,35 @@
 var myFullName;
 console.log(myFullName);
 
+
 myFullName="Pravin Mane"
 console.log(myFullName);
+
+myFullName="Pravin Vasant Mane"
+console.log(myFullName);
+console.log("-----------------------------");
+
+var myLove="I Love JS"
+console.log(myLove);
+console.log("-----------------------------");
+var firstName="First Name:Pravin"  
+console.log(firstName);
+
+var lastName="Last Name:Mane"
+console.log(lastName);
+
+var age="Age:28"
+console.log(age);
+
+var address="Address: Uwle, New Mumbai"
+console.log(address);
+
+var pinCode="Pin Code:425154"
+console.log(pinCode);
+
+var gender="Gender: Male"
+console.log(gender);
+
+var isMarried="Is Married: Yes"
+console.log(isMarried);
+
---
+++ 02_typeOf.js
@@ -0,0 +1,27 @@
+
+var bankName
+console.log(bankName);
+console.log(typeof bankName);
+bankName="SBI Bank"
+console.log(bankName);
+console.log(typeof bankName);
+
+console.log("-----------------------------------");
+
+var marks=marks=90%
+console.log(marks);
+console.log(typeof marks);
+
+var isWorking
+console.log(isWorking);
+isWorking=true
+console.log(typeof isWorking);
+console.log("-----------------------------------");
+
+var totalCount="Hundred and Seven"
+console.log(totalCount);
+console.log(typeof totalCount);
+totalCount=107
+console.log(totalCount);
+console.log(typeof totalCount);
+
