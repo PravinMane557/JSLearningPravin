41dc80379b17495644486d8660def7f8461b2f88
PravinMane557 <137971157+PravinMane557@users.noreply.github.com>
2023-07-22 02:19:26 +0530

    fun assignment
    fun assignment

--- 0001_practice.js
+++ 0001_practice.js
@@ -1,12 +1,29 @@
 
+function data(num1, num2){
+    console.log('My name is Pravin');
+    console.log('i am technical man');
+    console.log('before swap==>', 'num1-', num1,'num2-', num2);
+    var tam=num1
+    num1=num2
+    num2=tam
+    console.log ('after swap==>', 'num1-', num1, 'num2-', num2)
+}
+var num1=12
+var num2='null'
+var num3=225
+data(num1,num2, num3);
+console.log('-----------------------------');
+ var love=null
+ console.log(typeof love);
 
-var num1=100;
-var num2=200;
-console.log('Before Swap=','num1-', num1, 'num2-', num2);
-var temp=num1;
-num1=num2;
-console.log(temp, num1, num2);
-num2=temp
-console.log('After Swap=', 'num1-', num1, 'num2-', num2);
+ console.log('----------------------------------------------------------------------------------------');
 
-
+ function addition(arg1, arg2, arg3) {
+          console.log('value of-', arg1, arg2, arg3);
+         result= arg1 + arg2 + arg3
+         console.log('Result of Sum-',result);
+         console.log('--------------------------------------------');
+ }
+ addition(10, 20, 52)
+ addition(-20,562,-200025142)
+ addition('rahul', 20, 2)
