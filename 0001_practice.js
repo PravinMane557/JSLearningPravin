332c64786c2c08e4a8fdb2b7083f4244444f1140
PravinMane557 <137971157+PravinMane557@users.noreply.github.com>
2023-07-24 07:01:01 +0530

    assignments completed
    assignments completed

--- 0001_practice.js
+++ 0001_practice.js
@@ -1,29 +1,8 @@
 
-function data(num1, num2){
-    console.log('My name is Pravin');
-    console.log('i am technical man');
-    console.log('before swap==>', 'num1-', num1,'num2-', num2);
-    var tam=num1
-    num1=num2
-    num2=tam
-    console.log ('after swap==>', 'num1-', num1, 'num2-', num2)
-}
-var num1=12
-var num2='null'
-var num3=225
-data(num1,num2, num3);
-console.log('-----------------------------');
- var love=null
- console.log(typeof love);
 
- console.log('----------------------------------------------------------------------------------------');
 
- function addition(arg1, arg2, arg3) {
-          console.log('value of-', arg1, arg2, arg3);
-         result= arg1 + arg2 + arg3
-         console.log('Result of Sum-',result);
-         console.log('--------------------------------------------');
- }
- addition(10, 20, 52)
- addition(-20,562,-200025142)
- addition('rahul', 20, 2)
+var greet='good morning kiran'
+var sleec=greet.slice(1,11)
+console.log(sleec);
+var slit=greet.split('')
+console.log(`${slit}`);
---
+++ strigBasicAssignment.js
@@ -0,0 +1,16 @@
+
+console.log('------------------Step1--------------------');
+function stringBasic(){
+    var companyName
+    console.log(`my dream company name is - "isro"`);
+}
+stringBasic()
+console.log('------------------Step2--------------------');
+var hobby1='cricket'
+var hobby2='football'
+var hobby3='badminton'
+console.log(`Hobby1-${hobby1}, Hobby2-${hobby2}, Hobby3-${hobby3}`);
+console.log('------------------Step3--------------------');
+var sumOf=hobby1.length+hobby2.length+hobby3.length
+console.log(`Sum of total no of carector is- ${sumOf}`);
+console.log('------------------Assignment 2--------------------');
